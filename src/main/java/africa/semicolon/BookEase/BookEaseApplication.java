7b4c68626d2aeb79a274513168079cca5e428c32
Adeshina Qudus <qudusa55@gmail.com>
2024-04-11 16:24:07 +0100

    userCreatingAccountTest

---
+++ src/main/java/africa/semicolon/BookEase/BookEaseApplication.java
@@ -0,0 +1,13 @@
+package africa.semicolon.BookEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookEaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/africa/semicolon/BookEase/BookEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package africa.semicolon.BookEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
