bab2e9b4e366f6dce84bd056645dc43e83bf7304
Adeshina Qudus <qudusa55@gmail.com>
2024-06-11 16:52:39 +0100

    commit

--- src/test/java/africa/semicolon/BookEase/services/EventServiceTest.java
+++ src/test/java/africa/semicolon/BookEase/services/EventServiceTest.java
@@ -19,51 +19,51 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 public class EventServiceTest {
 
-//    @Autowired
-//    private EventService eventService;
-//    @Autowired
-//    private EventRepository repository;
-//
-//    @Each
-//    public void deleteAll() {
-//        repository.deleteAll();
-//    }
-//
-//    @Test
-//    public void userCreateEventTest(){
-//        CreateEventRequest request = new CreateEventRequest();
-//        request.setEventName("Mr Money With The Vibe Concert494");
-//        String dateInput = "01/13/2024";
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-//        LocalDate date = LocalDate.parse(dateInput,formatter);
-//        request.setDate(String.valueOf(date));
-//        request.setAvailableAttendees(0);
-//        request.setEventDescription("description");
-//        request.setCategory("concert");
-//        request.setUserEmail("djfemz22@gmail.com");
-//        CreateEventResponse response = eventService.createEvent(request);
-//        assertThat(response).isNotNull();
-//
-//    }
-//
-//    @Test
-//    public void searchEventTest(){
-//        CreateEventRequest request = new CreateEventRequest();
-//        request.setEventName("Mr Money With The Vibe Concert4400");
-//        String dateInput = "01/13/2024";
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-//        LocalDate date = LocalDate.parse(dateInput,formatter);
-//        request.setDate(String.valueOf(date));
-//        request.setAvailableAttendees(0);
-//        request.setEventDescription("description");
-//        request.setCategory("concert");
-//        request.setUserEmail("qudusa55@gmail.com");
-//        CreateEventResponse response = eventService.createEvent(request);
-//        SearchEventRequest searchEventRequest = new SearchEventRequest();
-//        searchEventRequest.setEventName("Mr Money With The Vibe Concert4400");
-//        SearchEventResponse searchEventResponse = eventService.searchEvent(searchEventRequest);
-//        assertThat(response).isNotNull();
-//    }
+    @Autowired
+    private EventService eventService;
+    @Autowired
+    private EventRepository repository;
+
+    @AfterEach
+    public void deleteAll() {
+        repository.deleteAll();
+    }
+
+    @Test
+    public void userCreateEventTest(){
+        CreateEventRequest request = new CreateEventRequest();
+        request.setEventName("Mr Money With The Vibe Concert494");
+        String dateInput = "01/13/2024";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        LocalDate date = LocalDate.parse(dateInput,formatter);
+        request.setDate(String.valueOf(date));
+        request.setAvailableAttendees(0);
+        request.setEventDescription("description");
+        request.setCategory("concert");
+        request.setUserEmail("djfemz22@gmail.com");
+        CreateEventResponse response = eventService.createEvent(request);
+        assertThat(response).isNotNull();
+
+    }
+
+    @Test
+    public void searchEventTest(){
+        CreateEventRequest request = new CreateEventRequest();
+        request.setEventName("Mr Money With The Vibe Concert4400");
+        String dateInput = "01/13/2024";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        LocalDate date = LocalDate.parse(dateInput,formatter);
+        request.setDate(String.valueOf(date));
+        request.setAvailableAttendees(0);
+        request.setEventDescription("description");
+        request.setCategory("concert");
+        request.setUserEmail("qudusa55@gmail.com");
+        CreateEventResponse response = eventService.createEvent(request);
+        SearchEventRequest searchEventRequest = new SearchEventRequest();
+        searchEventRequest.setEventName("Mr Money With The Vibe Concert4400");
+        SearchEventResponse searchEventResponse = eventService.searchEvent(searchEventRequest);
+        assertThat(response).isNotNull();
+    }
 
 
 }
--- src/test/java/africa/semicolon/BookEase/services/UserServiceTest.java
+++ src/test/java/africa/semicolon/BookEase/services/UserServiceTest.java
@@ -35,20 +35,20 @@ public class UserServiceTest {
     public void deleteAll(){
         userRepository.deleteAll();
     }
-//    @BeforeEach
-//    public void beforeEach(){
-//        CreateEventRequest request = new CreateEventRequest();
-//        request.setEventName("Mr Money With The Vibe Concert");
-//        String dateInput = "01/13/2024";
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-//        LocalDate date = LocalDate.parse(dateInput,formatter);
-//        request.setDate(String.valueOf(date));
-//        request.setAvailableAttendees(0);
-//        request.setEventDescription("description");
-//        request.setCategory("concert");
-//        request.setUserEmail("djfemz22@gmail.com");
-//        eventService.createEvent(request);
-//    }
+    @BeforeEach
+    public void beforeEach(){
+        CreateEventRequest request = new CreateEventRequest();
+        request.setEventName("Mr Money With The Vibe Concert");
+        String dateInput = "01/13/2024";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        LocalDate date = LocalDate.parse(dateInput,formatter);
+        request.setDate(String.valueOf(date));
+        request.setAvailableAttendees(0);
+        request.setEventDescription("description");
+        request.setCategory("concert");
+        request.setUserEmail("djfemz22@gmail.com");
+        eventService.createEvent(request);
+    }
 
     @Test
     public void userCreatingAccountTest(){
@@ -63,78 +63,78 @@ public class UserServiceTest {
         assertEquals("Account Created",response.getMessage());
     }
 
-//    @Test
-//    public void userCreateAccountWithInvalidEmailFormat(){
-//
-//        CreateAccountRequest request = new CreateAccountRequest();
-//
-//        request.setName("Femi");
-//        request.setEmail("djfemz22@gmail.");
-//        request.setPassword("Femzy12@");
-//
-//         assertThrows(InvalidMailFormatException.class,()->userService.createAccount(request));
-//    }
-//
-//    @Test
-//    public void userCreateAccountWithInvalidPasswordFormat(){
-//
-//        CreateAccountRequest request = new CreateAccountRequest();
-//
-//        request.setName("Femi");
-//        request.setEmail("djfemz22@gmail.com");
-//        request.setPassword("Femzy12");
-//
-//        assertThrows(InvalidPasswordFormatException.class,()->userService.createAccount(request));
-//    }
-//
-//    @Test
-//    public void userReserveTicketTest(){
-//
-//        ReserveTicketRequest reserveTicketRequest = new ReserveTicketRequest();
-//        reserveTicketRequest.setNumberOfReservedTicket(3);
-//        reserveTicketRequest.setEventName("Mr Money With The Vibe Concert");
-//        reserveTicketRequest.setAttendeesEmail("djfemz22@gmail.com");
-//
-//        ReserveTicketResponse ticketResponse = userService.reserveTicket(reserveTicketRequest);
-//
-//        assertThat(ticketResponse).isNotNull();
-//    }
-//
-//    @Test
-//    public void viewBookedEventTest(){
-//        ReserveTicketRequest reserveTicketRequest = new ReserveTicketRequest();
-//        reserveTicketRequest.setNumberOfReservedTicket(3);
-//        reserveTicketRequest.setEventName("Mr Money With The Vibe Concert");
-//        reserveTicketRequest.setAttendeesEmail("djfemz22@gmail.com");
-//        userService.reserveTicket(reserveTicketRequest);
-//
-//        ViewBookedEventRequest viewBookedEventRequest = new ViewBookedEventRequest();
-//
-//        viewBookedEventRequest.setUserEmail("djfemz22@gmail.com");
-//
-//       List<ViewBookedEventResponse> response =
-//                userService.viewBookedEvent(viewBookedEventRequest);
-//
-//        assertThat(response).isNotNull();
-//    }
-//
-//    @Test
-//    public void cancelReservationTest(){
-//        ReserveTicketRequest reserveTicketRequest = new ReserveTicketRequest();
-//        reserveTicketRequest.setNumberOfReservedTicket(3);
-//        reserveTicketRequest.setEventName("Mr Money With The Vibe Concert");
-//        reserveTicketRequest.setAttendeesEmail("djfemz22@gmail.com");
-//        userService.reserveTicket(reserveTicketRequest);
-//
-//        CancelReservationRequest cancelReservationRequest = new CancelReservationRequest();
-//        cancelReservationRequest.setNumberOfReservedTicket(2);
-//        cancelReservationRequest.setEventName("Mr Money With The Vibe Concert");
-//        cancelReservationRequest.setAttendeesEmail("djfemz22@gmail.com");
-//
-//        CancelReservationResponse response =
-//                userService.cancelReservation(cancelReservationRequest);
-//        assertThat(response).isNotNull();
-//
-//    }
+    @Test
+    public void userCreateAccountWithInvalidEmailFormat(){
+
+        CreateAccountRequest request = new CreateAccountRequest();
+
+        request.setName("Femi");
+        request.setEmail("djfemz22@gmail.");
+        request.setPassword("Femzy12@");
+
+         assertThrows(InvalidMailFormatException.class,()->userService.createAccount(request));
+    }
+
+    @Test
+    public void userCreateAccountWithInvalidPasswordFormat(){
+
+        CreateAccountRequest request = new CreateAccountRequest();
+
+        request.setName("Femi");
+        request.setEmail("djfemz22@gmail.com");
+        request.setPassword("Femzy12");
+
+        assertThrows(InvalidPasswordFormatException.class,()->userService.createAccount(request));
+    }
+
+    @Test
+    public void userReserveTicketTest(){
+
+        ReserveTicketRequest reserveTicketRequest = new ReserveTicketRequest();
+        reserveTicketRequest.setNumberOfReservedTicket(3);
+        reserveTicketRequest.setEventName("Mr Money With The Vibe Concert");
+        reserveTicketRequest.setAttendeesEmail("djfemz22@gmail.com");
+
+        ReserveTicketResponse ticketResponse = userService.reserveTicket(reserveTicketRequest);
+
+        assertThat(ticketResponse).isNotNull();
+    }
+
+    @Test
+    public void viewBookedEventTest(){
+        ReserveTicketRequest reserveTicketRequest = new ReserveTicketRequest();
+        reserveTicketRequest.setNumberOfReservedTicket(3);
+        reserveTicketRequest.setEventName("Mr Money With The Vibe Concert");
+        reserveTicketRequest.setAttendeesEmail("djfemz22@gmail.com");
+        userService.reserveTicket(reserveTicketRequest);
+
+        ViewBookedEventRequest viewBookedEventRequest = new ViewBookedEventRequest();
+
+        viewBookedEventRequest.setUserEmail("djfemz22@gmail.com");
+
+       List<ViewBookedEventResponse> response =
+                userService.viewBookedEvent(viewBookedEventRequest);
+
+        assertThat(response).isNotNull();
+    }
+
+    @Test
+    public void cancelReservationTest(){
+        ReserveTicketRequest reserveTicketRequest = new ReserveTicketRequest();
+        reserveTicketRequest.setNumberOfReservedTicket(3);
+        reserveTicketRequest.setEventName("Mr Money With The Vibe Concert");
+        reserveTicketRequest.setAttendeesEmail("djfemz22@gmail.com");
+        userService.reserveTicket(reserveTicketRequest);
+
+        CancelReservationRequest cancelReservationRequest = new CancelReservationRequest();
+        cancelReservationRequest.setNumberOfReservedTicket(2);
+        cancelReservationRequest.setEventName("Mr Money With The Vibe Concert");
+        cancelReservationRequest.setAttendeesEmail("djfemz22@gmail.com");
+
+        CancelReservationResponse response =
+                userService.cancelReservation(cancelReservationRequest);
+        assertThat(response).isNotNull();
+
+    }
 
 }
